60ed5d1acc500dc1bdb8d652bdc2625df9906927
Aagii <Aagii@10.10.11.66>
2017-05-23 15:43:45 -0500

    CheckLogin

--- src/controller/CheckLogin.java
+++ src/controller/CheckLogin.java
@@ -2,7 +2,13 @@ package controller;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,66 +18,37 @@ import javax.servlet.http.HttpSession;
 import dao.ConnectDB;
 import model.Users;
 
-/*@WebFilter("/*")
-class LoginFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {    
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpSession session = request.getSession(false);
-        String loginURI = request.getContextPath() + "/login";
-
-        boolean loggedIn = session != null && session.getAttribute("user") != null;
-        boolean loginRequest = request.getRequestURI().equals(loginURI);
-
-        if (loggedIn || loginRequest) {
-        	System.out.println("1");
-            chain.doFilter(request, response);
-        } else {
-        	System.out.println("2");
-            response.sendRedirect(loginURI);
-        }
-    }
-
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		//doFilter(arg0.getServletContext().req, res, chain);
-		
-	}
-
-    // ...
-}*/
 
 @WebServlet("/login")
 public class CheckLogin extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
 		ConnectDB db = new ConnectDB();
 		Users user = db.retrieveUserInfo(email, password);
-		System.out.println(user);
 		if(user != null){
+			System.out.println("logged");
 			HttpSession session=req.getSession();  
 	        session.setAttribute("userid",user.getUserid());
 	        LoginSession.addSession(session);
 	        req.setAttribute("message", "Welcome MUMJobLeadsProject");
 	        req.setAttribute("status", true);
+	        req.setAttribute("session", session);
 	        resp.sendRedirect("main.html");
 	        //req.getRequestDispatcher("main.html");
 		}
 		else{
+			System.out.println("notlogged");
 			req.setAttribute("message", "Please check email and password");
 			req.setAttribute("status", false);
-			 resp.sendRedirect("login.html");
+			resp.sendRedirect("login.html");
 		}
 	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
 }
+
